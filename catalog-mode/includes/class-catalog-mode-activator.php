cdad6ba0807548105a4bbf2eefba0ee7141ac9cf
Rajalaxmi77 <144342489+Rajalaxmi77@users.noreply.github.com>
2023-09-22 16:18:55 +0530

    Add files via upload

---
+++ catalog-mode/includes/class-catalog-mode-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://author.example.com/
+ * @since      1.0.0
+ *
+ * @package    Catalog_Mode
+ * @subpackage Catalog_Mode/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Catalog_Mode
+ * @subpackage Catalog_Mode/includes
+ * @author     Rajalaxmi <rajalaxmi7787@gmail.com>
+ */
+class Catalog_Mode_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
